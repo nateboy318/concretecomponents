27588faecc1e4b63109239e3c8698c4ad404fc16
nateboy318 <mcgamer5hundred@gmail.com.com>
2024-09-13 18:47:36 -0400

    fix for domain 7

--- src/blocks/hero.js
+++ src/blocks/hero.js
@@ -1,31 +1,34 @@
 import React from "react";
 import img1 from "../img/1.PNG";
 
-const hero = () => {
+const Hero = ({ data }) => {
+  // Extract the necessary fields from the data prop
+  const { MainText, SubText, ButtonText } = data;
+
   return (
     <div>
-      <div class="relative bg-gradient-to-r from-purple-600 to-blue-600 h-screen text-white overflow-hidden">
-        <div class="absolute inset-0">
+      <div className="relative bg-gradient-to-r from-purple-600 to-blue-600 h-screen text-white overflow-hidden">
+        <div className="absolute inset-0">
           <img
-            src={img1}
+            src={img1} // Use the URL from the BackgroundImage field
             alt="Background Image"
-            class="object-cover object-center w-full h-full"
+            className="object-cover object-center w-full h-full"
           />
-          <div class="absolute inset-0 bg-black opacity-50"></div>
+          <div className="absolute inset-0 bg-black opacity-50"></div>
         </div>
 
-        <div class="relative z-10 flex flex-col justify-center items-center h-full text-center">
-          <h1 class="text-6xl sm:text-[7em] font-bold leading-tight mb-4">
-            CONCRETE COMPONENTS
+        <div className="relative z-10 flex flex-col justify-center items-center h-full text-center px-4 sm:px-6 lg:px-8">
+          <h1 className="text-6xl sm:text-[7em] font-bold leading-tight mb-4">
+            {MainText} {/* Render MainText */}
           </h1>
-          <p class="sm:max-w-7xl max-w-xs text-xl sm:text-4xl text-gray-300 mb-16">
-            The Future of Durable Concrete for Your Coastal Home
+          <p className="sm:max-w-7xl max-w-xs text-xl sm:text-4xl text-gray-300 mb-16">
+            {SubText} {/* Render SubText */}
           </p>
           <a
-            href="/#contact"
-            class="bg-maroon text-white hover:bg-opacity-65 py-4 px-12 text-xl font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
+            href="#contact"
+            className="bg-maroon text-white hover:bg-opacity-65 py-4 px-12 text-xl font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
           >
-            Become A Dealer
+            {ButtonText} {/* Render ButtonText */}
           </a>
         </div>
       </div>
@@ -33,4 +36,4 @@ const hero = () => {
   );
 };
 
-export default hero;
+export default Hero;
